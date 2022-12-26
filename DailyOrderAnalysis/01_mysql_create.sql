7c7227be302a14112b90ef042e0feded6aa94530
mohamed nazm <63538356+mohamednazm-web@users.noreply.github.com>
2022-12-26 14:18:14 +0300

    Update 01_mysql_create.sql

--- DailyOrderAnalysis/01_mysql_create.sql
+++ DailyOrderAnalysis/01_mysql_create.sql
@@ -101,3 +101,19 @@ CREATE TABLE cust_order (
     CONSTRAINT fk_order_ship FOREIGN KEY (shipping_method_id) REFERENCES shipping_method (method_id),
     CONSTRAINT fk_order_addr FOREIGN KEY (dest_address_id) REFERENCES address (address_id)
 );
+
+CREATE TABLE order_status (
+    status_id INT,
+    status_value VARCHAR(20),
+    CONSTRAINT pk_orderstatus PRIMARY KEY (status_id)
+);
+
+CREATE TABLE order_line (
+    line_id INT AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    price DECIMAL(5, 2),
+    CONSTRAINT pk_orderline PRIMARY KEY (line_id),
+    CONSTRAINT fk_ol_order FOREIGN KEY (order_id) REFERENCES cust_order (order_id),
+    CONSTRAINT fk_ol_book FOREIGN KEY (book_id) REFERENCES book (book_id)
+);
