77f89dd2e598fe1b48a5acf7498d1ac5ea2d38c4
mohamed nazm <63538356+mohamednazm-web@users.noreply.github.com>
2022-12-26 13:48:00 +0300

    Update 01_mysql_create.sql

--- DailyOrderAnalysis/01_mysql_create.sql
+++ DailyOrderAnalysis/01_mysql_create.sql
@@ -65,3 +65,11 @@ CREATE TABLE address (
     CONSTRAINT pk_address PRIMARY KEY (address_id),
     CONSTRAINT fk_addr_ctry FOREIGN KEY (country_id) REFERENCES country (country_id)
 );
+
+CREATE TABLE customer (
+    customer_id INT,
+    first_name VARCHAR(200),
+    last_name VARCHAR(200),
+    email VARCHAR(350),
+    CONSTRAINT pk_customer PRIMARY KEY (customer_id)
+);
