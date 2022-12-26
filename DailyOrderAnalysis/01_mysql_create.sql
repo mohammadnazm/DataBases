cc7872ec11fdcafa2a4320b0add71abf0a62d9d8
mohamed nazm <63538356+mohamednazm-web@users.noreply.github.com>
2022-12-26 13:55:27 +0300

    create table customer address

--- DailyOrderAnalysis/01_mysql_create.sql
+++ DailyOrderAnalysis/01_mysql_create.sql
@@ -73,3 +73,12 @@ CREATE TABLE customer (
     email VARCHAR(350),
     CONSTRAINT pk_customer PRIMARY KEY (customer_id)
 );
+
+CREATE TABLE customer_address (
+    customer_id INT,
+    address_id INT,
+    status_id INT,
+    CONSTRAINT pk_custaddr PRIMARY KEY (customer_id, address_id),
+    CONSTRAINT fk_ca_cust FOREIGN KEY (customer_id) REFERENCES customer (customer_id),
+    CONSTRAINT fk_ca_addr FOREIGN KEY (address_id) REFERENCES address (address_id)
+);
