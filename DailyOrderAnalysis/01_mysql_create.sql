93387c5042d4b9bc33801c019b5a181f8b48d07a
mohamed nazm <63538356+mohamednazm-web@users.noreply.github.com>
2022-12-26 13:36:19 +0300

    create book table

--- DailyOrderAnalysis/01_mysql_create.sql
+++ DailyOrderAnalysis/01_mysql_create.sql
@@ -21,3 +21,17 @@ CREATE TABLE book_language (
     language_name VARCHAR(50),
     CONSTRAINT pk_language PRIMARY KEY (language_id)
 );
+
+
+CREATE TABLE book (
+    book_id INT,
+    title VARCHAR(400),
+    isbn13 VARCHAR(13),
+    language_id INT,
+    num_pages INT,
+    publication_date DATE,
+    publisher_id INT,
+    CONSTRAINT pk_book PRIMARY KEY (book_id),
+    CONSTRAINT fk_book_lang FOREIGN KEY (language_id) REFERENCES book_language (language_id),
+    CONSTRAINT fk_book_pub FOREIGN KEY (publisher_id) REFERENCES publisher (publisher_id)
+);
