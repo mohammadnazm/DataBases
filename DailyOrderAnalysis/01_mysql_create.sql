a6e3426b9d2c01f956aa870cff8eba31d9ba3129
mohamed nazm <63538356+mohamednazm-web@users.noreply.github.com>
2022-12-26 12:14:30 +0300

    create publisher table

--- DailyOrderAnalysis/01_mysql_create.sql
+++ DailyOrderAnalysis/01_mysql_create.sql
@@ -7,3 +7,10 @@ CREATE TABLE author (
     author_name VARCHAR(400),
     CONSTRAINT pk_author PRIMARY KEY (author_id)
 );
+
+
+CREATE TABLE PUBLISHER (
+	publisher_id INT,
+    publisher_name VARCHAR(400),
+    CONSTRAINT pk_publisher PRIMARY KEY (publisher_id)
+);
