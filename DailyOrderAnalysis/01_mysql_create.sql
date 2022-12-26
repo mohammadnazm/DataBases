943438751a94da04e3ec1f64fa4f2dc615349eda
mohamed nazm <63538356+mohamednazm-web@users.noreply.github.com>
2022-12-26 14:37:30 +0300

    Update 01_mysql_create.sql

--- DailyOrderAnalysis/01_mysql_create.sql
+++ DailyOrderAnalysis/01_mysql_create.sql
@@ -117,3 +117,13 @@ CREATE TABLE order_line (
     CONSTRAINT fk_ol_order FOREIGN KEY (order_id) REFERENCES cust_order (order_id),
     CONSTRAINT fk_ol_book FOREIGN KEY (book_id) REFERENCES book (book_id)
 );
+
+CREATE TABLE order_history (
+    history_id INT AUTO_INCREMENT,
+    order_id INT,
+    status_id INT,
+    status_date DATETIME,
+    CONSTRAINT pk_orderhist PRIMARY KEY (history_id),
+    CONSTRAINT fk_oh_order FOREIGN KEY (order_id) REFERENCES cust_order (order_id),
+    CONSTRAINT fk_oh_status FOREIGN KEY (status_id) REFERENCES order_status (status_id)
+);
