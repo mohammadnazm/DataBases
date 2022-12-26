763b1e6e5a8aaac7dfce1c206fefe048aefb001e
mohamed nazm <63538356+mohamednazm-web@users.noreply.github.com>
2022-12-26 13:47:17 +0300

    update

--- DailyOrderAnalysis/01_mysql_create.sql
+++ DailyOrderAnalysis/01_mysql_create.sql
@@ -55,3 +55,13 @@ CREATE TABLE country (
     country_name VARCHAR(200),
     CONSTRAINT pk_country PRIMARY KEY (country_id)
 );
+
+CREATE TABLE address (
+    address_id INT,
+    street_number VARCHAR(10),
+    street_name VARCHAR(200),
+    city VARCHAR(100),
+    country_id INT,
+    CONSTRAINT pk_address PRIMARY KEY (address_id),
+    CONSTRAINT fk_addr_ctry FOREIGN KEY (country_id) REFERENCES country (country_id)
+);
