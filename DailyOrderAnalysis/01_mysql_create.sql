6062433e56899eb339ddc59113e39a1016c74ff2
mohamed nazm <63538356+mohamednazm-web@users.noreply.github.com>
2022-12-26 13:43:08 +0300

    fix relationship

--- DailyOrderAnalysis/01_mysql_create.sql
+++ DailyOrderAnalysis/01_mysql_create.sql
@@ -40,6 +40,6 @@ CREATE TABLE book_author (
     book_id INT,
     author_id INT,
     CONSTRAINT pk_bookauthor PRIMARY KEY (book_id, author_id),
-    CONSTRAINT fk_ba_book FOREIGN KEY (book_id) REFERENC book (book_id),
-    CONSTRAINT fk_ba_author FOREIGN KEY (author_id) REFERENC author (author_id)
+    CONSTRAINT fk_ba_book FOREIGN KEY (book_id) REFERENCES book (book_id),
+    CONSTRAINT fk_ba_author FOREIGN KEY (author_id) REFERENCES author (author_id)
 );
